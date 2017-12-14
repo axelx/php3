19ed3e2f0551f9080bcdc14ab10413ba56c08c2f
Jenkins <jenkins@domain.com>
2017-12-14 16:43:50 +0300

    костыльный вывод имени домена в шаблон

--- protected/Controllers/Index.php
+++ protected/Controllers/Index.php
@@ -2,6 +2,7 @@
 
 namespace App\Controllers;
 
+use T4\Core\Config;
 use T4\Mvc\Controller;
 
 class Index
@@ -11,6 +12,12 @@ class Index
     public function actionDefault()
     {
 
+        $tt = include ROOT_PATH_PROTECTED . '/config.php';
+
+        $this->data->domain = $tt["domain_name"];
+
+        $this->data->foo = 'bar';
     }
+
 
 }
--- protected/config.php
+++ protected/config.php
@@ -1,5 +1,5 @@
 <?php
 
 return [
-    'domain'=>"php3",
+    'domain_name'=>"php3_",
 ];
