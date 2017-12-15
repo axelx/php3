0ef188b3f6038c394024fad348b8d032d01995f6
Jenkins <jenkins@domain.com>
2017-12-15 09:49:39 +0300

    ДЗ №1 готово. build.xml - готов

--- protected/Controllers/Index.php
+++ protected/Controllers/Index.php
@@ -3,20 +3,21 @@
 namespace App\Controllers;
 
 use T4\Core\Config;
+use T4\Core\Std;
 use T4\Mvc\Controller;
 
 class Index
     extends Controller
 {
 
+
+
     public function actionDefault()
     {
 
-        $tt = include ROOT_PATH_PROTECTED . '/config.php';
+        $this->data->domain = $this->app->config->domain_name;
 
-        $this->data->domain = $tt["domain_name"];
 
-        $this->data->foo = 'bar';
     }
 
 
--- public/index.php
+++ public/index.php
@@ -4,9 +4,6 @@ require __DIR__ . '/../vendor/autoload.php';
 require __DIR__ . '/../protected/boot.php';
 require __DIR__ . '/../protected/autoload.php';
 
-//var_dump(ROOT_PATH_PROTECTED . '/config.php');
-//$tt = include ROOT_PATH_PROTECTED . '/config.php';
-//var_dump($tt);
 T4\Mvc\Application
     ::instance()
     ->setConfig(
