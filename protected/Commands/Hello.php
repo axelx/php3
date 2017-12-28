60a27a2d670507b638ba6328fcb90e312109b9aa
alChe <jenkins@domain.com>
2017-12-28 14:25:51 +0300

    ***

--- protected/Commands/Hello.php
+++ protected/Commands/Hello.php
@@ -10,8 +10,35 @@ class Hello extends Command
     public function actionDefault()
     {
 //        echo "Hello";
-       $name = $this->read('What is your name?');
-       $this->writeLn('Hello ' . $name . '!');
+//       $name = $this->read('What is your name?');
+//       $this->writeLn('Hello ' . $name . '!');
+
+
+/*        echo memory_get_usage(true);
+        echo "\n";
+
+        $arr = range(0,1000000);
+        echo memory_get_usage(true);
+
+        echo "\n";*/
+
+
+//        xdebug_debug_zval('x');
+
+        $x = 55;
+        echo $x . PHP_EOL;
+
+        $y = &$x;
+
+        $x = 20;
+
+        echo $y.PHP_EOL;
+
+        $y = 31;
+
+        echo $x . PHP_EOL;
+
+
 
     }
 
